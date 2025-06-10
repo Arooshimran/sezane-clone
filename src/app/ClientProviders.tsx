d94c8e4884054d09cf4ad8942116aacd948d0422
Aroosh Imran <arooshimran555@gmail.com>
2025-06-10 16:07:26 +0500

    changes made

--- src/app/ClientProviders.tsx
+++ src/app/ClientProviders.tsx
@@ -1,6 +1,6 @@
-"use client";
-import { CartProvider } from "./context/cartContext";
+// "use client";
+// import { CartProvider } from "./context/cartContext";
 
-export default function ClientProviders({ children }: { children: React.ReactNode }) {
-  return <CartProvider>{children}</CartProvider>;
-}
+// export default function ClientProviders({ children }: { children: React.ReactNode }) {
+//   return <CartProvider>{children}</CartProvider>;
+// }
